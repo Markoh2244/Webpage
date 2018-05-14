991a47a360e61961f477d8b4c8c01a23db723300
Marko Henien <markoh2244@hstat.org>
2018-05-14 18:50:21 +0000

    message

--- script.js
+++ script.js
@@ -1,5 +1,40 @@
 $(document).ready(function(){
-    $(".sustanince").hover(function(){
-        $(this).css("background-color", "yellow");
+    $(".sustenance").hover(function(){
+     $(this).css("background-color", "yellow");
+    }, function(){
+    $(this).css("background-color", "white");
         });
+});
+
+$('audio').each(function(){
+    if ($(this).is(":in-viewport")) {
+        $(this)[0].play();
+    } else {
+        $(this)[0].pause();
+    }
+})
+$(document).ready(function(){
+
+ 
+
+    $(window).scroll(function () {  //Listen for the window's scroll event
+        if (isScrolledAfterElement("#foods")) { //if it has scrolled beyond the #content elment
+            $('.navbar').fadeOut();  //Show the navigation bar
+        } else {
+            $('.navbar').fadeIn(); //Else hide it
+        }
+    });
+
+    //Function that returns true if the window has scrolled beyond the given element
+    function isScrolledAfterElement(elem) {
+        var $elem = $(elem);
+        var $window = $(window);
+
+        var docViewTop = $window.scrollTop();
+        var docViewBottom = docViewTop + $window.height();
+
+        var elemTop = $elem.offset().top;
+
+        return elemTop <= docViewBottom;
+    }
 });
